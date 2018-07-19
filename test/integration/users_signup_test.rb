963e687659045cedf1296919e62401b689510285
EC2 Default User <ec2-user@ip-172-31-37-39.us-east-2.compute.internal>
2018-07-19 07:06:15 +0000

    Implenment basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,8 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    # assert_select 'div#<CSS id for error explanation>'
-    # assert_select 'div.<CSS class for field with error>'
   end
   
   test "valid signup information" do
@@ -23,9 +20,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
+    # redirect_to @user
     follow_redirect!
+    # Test
     assert_template 'users/show'
-    # assert_not flash.FILL_IN
+    assert is_logged_in?
   end
-  
 end
