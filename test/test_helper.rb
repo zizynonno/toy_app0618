2f8d8384c034bb5df5ebffce37ffa61d5a29d2f0
EC2 Default User <ec2-user@ip-172-31-37-39.us-east-2.compute.internal>
2018-07-29 23:18:55 +0000

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,27 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+ENV['RAILS_ENV'] ||= 'test'
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  
-    # テストユーザーがログイン中の場合にtrueを返す
+
+  # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Add more helper methods to be used by all tests here...
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
